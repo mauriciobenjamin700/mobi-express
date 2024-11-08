870a8aaf88b020f53540520fe61d4bdd8d791997
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2024-11-08 18:53:53 -0300

    start project

---
+++ src/styles/global.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    :root{
+        --primary: #F7F9FC;
+        --secondary: #15181C;
+        --gray: #D9D9D9;
+        --blue: #4766FF;
+        --white: #FFFFFF;
+    }
+    body{
+        background-color: var(--primary);
+        color: var(--secondary);
+        font-size: 1.2rem;
+        font-weight: 400;
+        font-family: 'Roboto', sans-serif;
+    }
+    ul{list-style-type: none;}
+    a{text-decoration: none;}
+
+`
